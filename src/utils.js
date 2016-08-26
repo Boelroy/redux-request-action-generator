3daea472866d576a9f415c8b84c4aabced45485c
boelroy <boelroy@live.com>
2016-08-27 02:16:21 +0800

    修改支持多参数

--- src/utils.js
+++ src/utils.js
@@ -3,17 +3,22 @@
 import { toPairs, isArray } from 'lodash';
 
 // 目前不支持多参数
-export function serilizeQuery(query) {
-  return toPairs(query).reduce(function(iter, item) {
-
-    let query;
-
+export function serializeQuery(query) {
+  return toPairs(query).reduce((iter, item) => {
+    let result;
+    if (!item[1]) {
+      return iter;
+    }
     if (isArray(item[1])) {
-      query = item[1].reduce((iter, param) => `${iter+item[0]}[]=${param}&`, '');
+      result = item[1].reduce((iter1, param) => `${iter1 + item[0]}[]=${param}&`, '');
+    } else if (typeof item[1] === 'undefined') {
+      result = `${encodeURIComponent(item[0])}=&`;
+    } else if (typeof item[1] === 'object') {
+      result = `${encodeURIComponent(item[0])}=${encodeURIComponent(JSON.stringify(item[1]))}&`;
     } else {
-      query = item[0] + '='+ item[1]+'&';
+      result = `${encodeURIComponent(item[0])}=${encodeURIComponent(item[1])}&`;
     }
 
-    return iter+ query;
+    return iter + result;
   }, '?').slice(0, -1);
 }
