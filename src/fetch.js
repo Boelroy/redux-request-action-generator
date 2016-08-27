9607814305231173ea9989494ddcf06232db9f03
boelroy <boelroy@live.com>
2016-08-27 16:50:57 +0800

    支持then

--- src/fetch.js
+++ src/fetch.js
@@ -1,49 +1,56 @@
-'use strict';
-
 import fetch from 'isomorphic-fetch';
-import { serilizeQuery } from './utils';
+import { serializeQuery } from './utils';
 
 export default function commonFetch(options) {
-  const { requestAction, receiveAction, errorAction, method='GET', type='json' } = options;
-  let { url, data } = options,
-    headers = {};
-
-
-  console.log(url, method);
+  const { requestAction, receiveAction, errorAction, method = 'GET', type = 'json' } = options;
+  let { url, data } = options;
+  let headers = {};
   // 处理参数
-  switch(method) {
+  switch (method) {
     case 'GET':
-      url = url + serilizeQuery(data);
+      url = url + serializeQuery(data);
       break;
     case 'POST':
-      data = serilizeQuery(data).slice(1);
+      data = serializeQuery(data).slice(1);
       headers = {
-        'Content-Type':'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
       };
       break;
-  }
+    default:
 
+  }
+  const ops = {
+    method,
+    credentials: 'same-origin',
+    headers,
+  };
+  if (method !== 'GET') {
+    ops.body = data;
+  }
   return dispatch => {
     dispatch(requestAction());
-    return fetch(url, {
-      method,
-      body: data,
-      credentials: 'same-origin',
-      headers: headers
-    })
+    return fetch(url, ops)
     .then(response => {
-      if ( response.state >= 400) {
+      if (response.status >= 400) {
         // Error Handle
-        throw new Error('Bad response frin server');
+        throw new Error('网络问题');
       }
       return response[type]();
     })
-    .then(json => {
-      json = type === 'json' ? json : JSON.parse(json);
-      return dispatch(receiveAction(json));
-    }, error=> {
-      console.log(error);
-      return dispatch(errorAction(error));
-    });
+    .then(
+      json => {
+        const result = type === 'json' ? json : JSON.parse(json);
+        if (result.errno !== undefined && result.errno) {
+          dispatch(errorAction(result.errmsg, result.errno));
+          return Promise.reject(result);
+        }
+        return dispatch(receiveAction(result));
+      },
+
+      error => {
+        dispatch(errorAction(error.toString(), -1));
+        throw error;
+      }
+    );
   };
 }
--- src/index.js
+++ src/index.js
@@ -1,67 +1,92 @@
-'use strict';
 /*
  * 通用的异步请求的action和reducer
  *
  */
 
 import fetch from './fetch';
-import {result} from 'lodash';
+import { result, isFunction } from 'lodash';
 
-const commonRequestPrefix = 'COMMON_FETCH',
-  commonReceivePrefix = 'COMMON_FETCH_SUCCESS';
+const commonRequestPrefix = 'COMMON_FETCH';
+const commonReceiveMorePrefix = 'COMMON_FETCH_SUCCESS_MORE';
+const commonReceivePrefix = 'COMMON_FETCH_SUCCESS';
 
-function requestGenerator(id,name) {
-  return function(json){
-    return {
-      type: `${commonRequestPrefix}_${id.toUpperCase()}_${name.toUpperCase()}`,
-      json
-    };
-  };
+function requestGenerator(id, name) {
+  return (json) => ({
+    type: `${commonRequestPrefix}_${id.toUpperCase()}_${name.toUpperCase()}`,
+    json,
+  });
+}
+
+function receiveMoreGenerator(id, name, selector, listSelector) {
+  return (json) => ({
+    type: `${commonReceiveMorePrefix}_${id.toUpperCase()}_${name.toUpperCase()}`,
+    listSelector,
+    selector,
+    json,
+    name,
+  });
 }
 
 function receiveGenerator(id, name, selector) {
-  return function(json){
-    return {
-      type: `${commonReceivePrefix}_${id.toUpperCase()}_${name.toUpperCase()}`,
-      name,
-      selector,
-      json
-    };
-  };
+  return (json) => ({
+    type: `${commonReceivePrefix}_${id.toUpperCase()}_${name.toUpperCase()}`,
+    name,
+    selector,
+    json,
+  });
 }
 
-export const generator = (id) => (name, url, method='GET', selector='data') => {
-  return {
-    request: function(data,type) {
-      return fetch({
-        method,
-        url,
-        data,
-        requestAction: requestGenerator(id, name),
-        receiveAction: receiveGenerator(id, name, selector)
-      }, type);
+export const generator = (id) => (name, url, method = 'GET', selector = 'data') => ({
+  request: (data, type) => fetch({
+    method,
+    url,
+    data,
+    requestAction: requestGenerator(id, name),
+    receiveAction: receiveGenerator(id, name, selector),
+    type,
+  }),
+  requestMore: (data, listSelector) => fetch({
+    method,
+    url,
+    data,
+    requestAction: requestGenerator(id, name),
+    receiveAction: receiveMoreGenerator(id, name, selector, listSelector),
+  }),
+});
+
+
+export const reducer = (id) => (defaultState = {}, custormReducer) =>
+  (state = defaultState, action) => {
+    const { type } = action;
+    if (!type.startsWith) return state;
+
+    if (type.startsWith(`${commonRequestPrefix}_${id.toUpperCase()}`)) { // request
+      return {
+        ...state,
+        isFetching: true,
+      };
+    } else if (type.startsWith(`${commonReceivePrefix}_${id.toUpperCase()}`)) { // receive
+      return {
+        ...state,
+        isFetching: false,
+        [action.name]: result(action.json, action.selector, action.json),
+      };
+    } else if (type.startsWith(`${commonReceiveMorePrefix}_${id.toUpperCase()}`)) {
+      const newJson = result(action.json, action.selector, action.json);
+      return {
+        ...state,
+        isFetching: false,
+        [action.name]: {
+          ...state[action.name],
+          [action.listSelector]: [
+            ...state[action.name][action.listSelector],
+            ...newJson[action.listSelector],
+          ],
+        },
+      };
     }
+    if (isFunction(custormReducer)) {
+      return custormReducer(state, action);
+    }
+    return state;
   };
-};
-
-
-export const reducer = (id) => (defaultState={}) => (state=defaultState, action) => {
-
-  const { type }  = action;
-  if (!type.startsWith) return state;
-
-  if (type.startsWith(`${commonRequestPrefix}_${id.toUpperCase()}`)) { // request
-
-    return Object.assign({},  state, {
-      isFetching: true
-    });
-
-  } else if(type.startsWith(`${commonReceivePrefix}_${id.toUpperCase()}`)) { // receive
-    return Object.assign({},  state, {
-      isFetching: false,
-      [action.name]: result(action.json, action.selector, action.json)
-    });
-
-  }
-  return state;
-};
